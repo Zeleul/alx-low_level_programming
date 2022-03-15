c0c0484c5d2236aedfe5abca9c9f2ed674069267
zeleul <zeleult@gmail.com>
2022-03-15 14:53:31 +0300

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,46 +1,28 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - Prints the n times table
+ * print_to_98 - Prints all natural numbers
+ * from n to 98.
  *
- * @n: number times table (0 < n <= 15)
+ * @n: input number.
  *
- * Return: no return
+ * Return: no return.
  */
-void print_times_table(int n)
+void print_to_98(int n)
 {
-	int a, b, op;
-
-	if (n >= 0 && n <= 15)
+	if (n > 98)
 	{
-		for (a = 0; a <= n; a++)
+		for (; n > 98; n--)
 		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
-			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
-				}
-				else if (op <= 99)
-				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-			}
-			_putchar('\n');
+			printf("%d, ", n);
 		}
 	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
 }
