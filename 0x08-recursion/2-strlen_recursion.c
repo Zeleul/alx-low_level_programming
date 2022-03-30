af9e72496c611e50fccbd3b66276f1a9ca995f95
zeleul <zelulet@gmail.com>
2022-03-30 14:40:24 +0300

    Dreams feel real while we're in them.

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of as string
+ *
+ * @s: string to receive
+ *
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
+}
